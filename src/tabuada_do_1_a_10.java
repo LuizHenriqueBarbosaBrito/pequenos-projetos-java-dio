5fb120b9178a2dcc82f73378c7debaa31c66b71c
Luiz Henrique Barbosa Brito <luizhenriquebarbosabrito52@gmail.com>
2025-08-11 15:46:59 -0300

    Initial commit

---
+++ src/tabuada_do_1_a_10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class tabuada_do_1_a_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número;");
+        int number = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            int result = number * i;
+            System.out.println(number + "x" + i + "=" + result);
+        }
+    }
+}
